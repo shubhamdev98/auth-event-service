50cf948c7e8dbc1a18c45c3b840184aae4d936ab
shubhamdev98 <shubham.pn.1998@gmail.com>
2025-05-11 16:51:28 +0530

    fix error handling

--- src/validation/auth.validation.js
+++ src/validation/auth.validation.js
@@ -16,18 +16,18 @@ const signupValidationSchema = (req, res, next) => {
         }),
         email: Joi.string().email().required().messages({
             'string.email': 'Invalid email format',
-            'any.required': 'Email is required',
+            'string.empty': 'Email is required',
         }),
         password: Joi.string().min(6).required().messages({
             'string.min': 'Password must be at least 6 characters',
-            'any.required': 'Password is required',
+            'string.empty': 'Password is required',
         }),
         repeat_password: Joi.string()
             .valid(Joi.ref('password'))
             .required()
             .messages({
                 'any.only': 'Password and repeat password do not match',
-                'any.required': 'Repeat password is required',
+                'string.empty': 'Repeat password is required',
             }),
     })
 
